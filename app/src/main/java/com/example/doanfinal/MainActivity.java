3e28366ff80bc22872a59ca1f6077de5d91997d5
Miracle30 <lapaix3011@gmail.com>
2020-12-26 16:49:31 +0700

    XOg chuc nang thoat

--- app/src/main/java/com/example/doanfinal/MainActivity.java
+++ app/src/main/java/com/example/doanfinal/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.doanfinal;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.Notes:
+                //aa
+                break;
+            case R.id.Calendar:
+                //bb
+                break;
+            case R.id.About:
+                //cc
+                break;
+            case R.id.Exit:
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.exit_caption)
+                        .setMessage(R.string.exit_content)
+                        .setNegativeButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                finish();
+                            }
+                        })
+                        .setPositiveButton(R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                return;
+                            }
+                        })
+                        .show();
+
+                break;
+        }
+        return true;
     }
 }
