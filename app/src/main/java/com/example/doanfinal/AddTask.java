9571c091eda956976909be830cd6b6f5a823c2b7
Miracle30 <lapaix3011@gmail.com>
2021-01-05 09:49:41 +0700

    Add Item

--- app/src/main/java/com/example/doanfinal/AddTask.java
+++ app/src/main/java/com/example/doanfinal/AddTask.java
@@ -1,6 +1,7 @@
 package com.example.doanfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
@@ -13,10 +14,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import com.example.todo.DatePickerFrag;
+
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -24,11 +30,10 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class AddTask extends AppCompatActivity {
+public class AddTask extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
-    Calendar calendar;
-    TextView date, time, title;
-    EditText edtTask;
+    ImageView imvCalendar, imvSave;
+    TextView tvDate;
 
 
 
@@ -37,25 +42,39 @@ public class AddTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
         addControls();
+        addEvents();
+    }
+
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        //format
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+        tvDate.setText(currentDateString);
     }
 
     private void addControls() {
-        date = findViewById(R.id.date);
-        time = findViewById(R.id.time);
-        calendar = Calendar.getInstance();
+        imvCalendar = findViewById(R.id.imvCalendar);
+        imvSave = findViewById(R.id.imvSave);
+        tvDate = findViewById(R.id.tvDate);
+    }
 
-//        year = calendar.get(Calendar.YEAR);
-//        month = calendar.get(Calendar.MONTH);
-//        day = calendar.get(Calendar.DAY_OF_MONTH);
-//        hour = calendar.get(Calendar.HOUR_OF_DAY);
-//        minute = calendar.get(Calendar.MINUTE);
-
-        title = findViewById(R.id.title);
-        edtTask = findViewById(R.id.edtTask);
-
+    private void addEvents() {
+        imvCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFrag();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
 
     }
-    public void save(View view) {
 
-    }
+
 }
