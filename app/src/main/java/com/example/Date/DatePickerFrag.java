d1e681993e37d0990ce03198a1761a0d5754a524
Miracle30 <lapaix3011@gmail.com>
2021-01-06 23:59:15 +0700

    ddooir ti

--- app/src/main/java/com/example/Date/DatePickerFrag.java
+++ app/src/main/java/com/example/Date/DatePickerFrag.java
@@ -1,4 +1,4 @@
-package com.example.todo;
+package com.example.Date;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
--- app/src/main/java/com/example/doanfinal/AddNote.java
+++ app/src/main/java/com/example/doanfinal/AddNote.java
@@ -3,40 +3,27 @@ package com.example.doanfinal;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
-import com.example.todo.DatePickerFrag;
+import com.example.Date.DatePickerFrag;
 
 import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-
-import android.os.Bundle;
 
 public class AddNote extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
