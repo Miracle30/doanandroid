d8c65fd224d622526d8f5433a56154a6d5953dc5
hailongqng <kianhcr7@gmail.com>
2021-01-07 22:13:11 +0700

    Tran Hoang Hai LOng

--- app/src/main/java/com/example/doanfinal/AddNote.java
+++ app/src/main/java/com/example/doanfinal/AddNote.java
@@ -1,45 +1,37 @@
 package com.example.doanfinal;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-
-import android.app.DatePickerDialog;
+import android.app.TabActivity;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.Date.DatePickerFrag;
-
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
-public class AddNote extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class AddNote extends TabActivity {
 
-    ImageView imvCalendar;
-    TextView tvDate;
+    private List<Notes> ListNote = new ArrayList<Notes>();
     NotesAdapter adapter = null;
-    List<Notes> NotesList = new ArrayList<Notes>();
-
-    public class NotesAdapter extends ArrayAdapter<Notes>{
-        public NotesAdapter(Context context,int textViewResourceId){
+    class NotesAdapter extends ArrayAdapter<Notes> {
+        public NotesAdapter(Context context, int textViewResourceId) {
             super(context, textViewResourceId);
         }
-        public NotesAdapter()
-        {
-            super(AddNote.this, android.R.layout.simple_list_item_1,NotesList);
+
+        public NotesAdapter() {
+            super(AddNote.this, android.R.layout.simple_list_item_1, ListNote);
         }
+
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             //TODO Auto-generated method
@@ -48,72 +40,68 @@ public class AddNote extends AppCompatActivity implements DatePickerDialog.OnDat
                 LayoutInflater inflater = getLayoutInflater();
                 row = inflater.inflate(R.layout.listnote, null);
             }
-            Notes r = NotesList.get(position);
-            ((TextView) row.findViewById(R.id.titlethemvao)).setText(r.getTitle());
+            Notes n = ListNote.get(position);
+            ((TextView) row.findViewById(R.id.title)).setText(n.getNote());
+            ((TextView) row.findViewById(R.id.content)).setText(n.getContent());
             return row;
         }
     }
+    private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            Notes r = ListNote.get(position); // lấy item được chọn
+            EditText note;
+            EditText content;
+// Tham chiếu đến các view trong addtask
+            note = (EditText)findViewById(R.id.title3);
+            content = (EditText)findViewById(R.id.addnotes);
+// thiết lập thông tin tương ứng
+            note.setText(r.getNote());
+            content.setText(r.getContent());
+// sinh viên có thể bổ sung lệnh sau để chuyển view về tab details
+            getTabHost().setCurrentTab(1);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
         Button save = (Button) findViewById(R.id.Save);
         save.setOnClickListener(onSave);
-        ListView list = (ListView)findViewById(R.id.listnote123);
+        ListView list = (ListView)findViewById(R.id.notes);
+        list.setOnItemClickListener(onListClick);
         adapter = new NotesAdapter();
         list.setAdapter(adapter);
 
-        addControls();
-        addEvents();
 
-    }
+        TabHost.TabSpec spec = getTabHost().newTabSpec("tag1");
+        spec.setContent(R.id.notes);
+        spec.setIndicator("List",getResources().getDrawable(R.drawable.list));
+        getTabHost().addTab(spec);
 
-    //save
+        spec = getTabHost().newTabSpec("tag2");
+        spec.setContent(R.id.AddTask);
+        spec.setIndicator("Details",
+                getResources().getDrawable(R.drawable.notes));
+        getTabHost().addTab(spec);
 
+        getTabHost().setCurrentTab(0);
 
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.YEAR, year);
-        c.set(Calendar.MONTH, month);
-        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-
-        //format
-        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
-        tvDate.setText(currentDateString);
-    }
-
-    private void addControls() {
-        imvCalendar = findViewById(R.id.imvCalendar);
-//        imvSave = findViewById(R.id.imvSave);
-        tvDate = findViewById(R.id.tvDate);
-    }
-//
-
-    private void addEvents() {
-        imvCalendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DialogFragment datePicker = new DatePickerFrag();
-                datePicker.show(getSupportFragmentManager(), "date picker");
-            }
-        });
     }
     private View.OnClickListener onSave = new View.OnClickListener() {
         public void onClick(View v) {
+            String msg = "Successfully add to notes";
             Notes r = new Notes();
-
-            String msg;
-            EditText title = findViewById(R.id.title);
-            r.setTitle(title.getText().toString());
-            msg = title.getText().toString();
-            Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
-            NotesList.add(r);
+            EditText note = findViewById(R.id.title3);
+            EditText content = findViewById(R.id.addnotes);
+            r.setNote(note.getText().toString());
+            r.setContent(content.getText().toString());
+            ListNote.add(r);
         }
-
-
     };
-
 
 
 
--- app/src/main/java/com/example/doanfinal/Notes.java
+++ app/src/main/java/com/example/doanfinal/Notes.java
@@ -1,15 +1,26 @@
 package com.example.doanfinal;
 
 public class Notes {
-    private String title="";
-    public String getTitle() {
-        return title;
+    private static String note="";
+
+    public static String getContent() {
+        return content;
     }
-    public void setTitle(String title) {
-        this.title = title;
+
+    public static void setContent(String content) {
+        Notes.content = content;
+    }
+
+    private static String content="";
+
+    public static String getNote() {
+        return note;
+    }
+    public void setNote(String note) {
+        this.note = note;
     }
     public Notes(){ }
     public String toString(){
-    return(getTitle());//tra ve ten title
+    return(getNote());//tra ve ten title
 }
 }
